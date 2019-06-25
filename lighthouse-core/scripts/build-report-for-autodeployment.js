efd41c9544efaaa5f2fea26098cfcf090a39dcf7
Paul Irish <paulirish@google.com>
2019-06-25 15:23:20 -0700

    misc(build): create error-y LHR for the deploy

--- lighthouse-core/scripts/build-report-for-autodeployment.js
+++ lighthouse-core/scripts/build-report-for-autodeployment.js
@@ -14,32 +14,31 @@ const mkdirp = require('mkdirp').sync;
 const swapLocale = require('../lib/i18n/swap-locale.js');
 
 const ReportGenerator = require('../../lighthouse-core/report/report-generator.js');
+const GatherRunner = require('../gather/gather-runner.js');
+const {defaultSettings} = require('../config/constants.js');
+const lighthouse = require('../index.js');
 const lhr = /** @type {LH.Result} */ (require('../../lighthouse-core/test/results/sample_v2.json'));
 
 const DIST = path.join(__dirname, `../../dist`);
 
-// Add a plugin to demo plugin rendering.
-lhr.categories['lighthouse-plugin-someplugin'] = {
-  id: 'lighthouse-plugin-someplugin',
-  title: 'Plugin',
-  score: 0.5,
-  auditRefs: [],
-};
-
 (async function() {
-  const filenameToLhr = {
-    'english': lhr,
-    'espanol': swapLocale(lhr, 'es').lhr,
-    'arabic': swapLocale(lhr, 'ar').lhr,
-  };
+  addPluginCategory(lhr);
+  const errorLhr = await generateErrorLHR();
 
-  mkdirp(DIST);
+  const filenameToLhr = {
+    english: lhr,
+    espanol: swapLocale(lhr, 'es').lhr,
+    arabic: swapLocale(lhr, 'ar').lhr,
+    error: errorLhr,
+  };
 
   // Generate and write reports
   Object.entries(filenameToLhr).forEach(([filename, lhr]) => {
     let html = ReportGenerator.generateReportHtml(lhr);
+    // TODO: PSI is another variant to consider
     for (const variant of ['', '-devtools']) {
       if (variant === '-devtools') {
+        // TODO: Make the DevTools Audits panel "emulation" more comprehensive
         html = html.replace(`"lh-root lh-vars"`, `"lh-root lh-vars lh-devtools"`);
       }
       const filepath = `${DIST}/${filename}${variant}/index.html`;
@@ -49,3 +48,40 @@ lhr.categories['lighthouse-plugin-someplugin'] = {
     }
   });
 })();
+
+/**
+ * Add a plugin to demo plugin rendering.
+ * @param {LH.Result} lhr
+ */
+function addPluginCategory(lhr) {
+  lhr.categories['lighthouse-plugin-someplugin'] = {
+    id: 'lighthouse-plugin-someplugin',
+    title: 'Plugin',
+    score: 0.5,
+    auditRefs: [],
+  };
+}
+
+/**
+ * Generate an LHR with errors for the renderer to display
+ * We'll write an "empty" artifacts file to disk, only to use it in auditMode
+ * @return {Promise<LH.Result>}
+ */
+async function generateErrorLHR() {
+  const url = 'http://fakeurl.com';
+  const options = {
+    requestedUrl: url,
+    settings: defaultSettings,
+    driver: {
+      getBrowserVersion: () => ({userAgent: 'Mozilla/5.0 ErrorUserAgent Chrome/66'}),
+    },
+  };
+  //@ts-ignore driver isn't mocked out completely
+  const artifacts = await GatherRunner.initializeBaseArtifacts(options);
+
+  const TMP = `${DIST}/.tmp/`;
+  mkdirp(TMP);
+  fs.writeFileSync(`${TMP}/artifacts.json`, JSON.stringify(artifacts), 'utf-8');
+  const errorRunnerResult = await lighthouse(url, {auditMode: TMP});
+  return /** @type {LH.RunnerResult} */ (errorRunnerResult).lhr;
+}
